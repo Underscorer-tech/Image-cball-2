f2207f5428f76a453455b139cb1c3c0bc8916165
Underscorer-tech <78732817+Underscorer-tech@users.noreply.github.com>
2021-03-26 10:59:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var d1,dimg;
 var ba,wall1,wall2,wall3,ground,ball1;
 function preload()
 {
-	
+	dimg = loadImage("dustbin.jfif");
 }
 
 function setup() {
@@ -17,29 +18,31 @@ function setup() {
  wall1 = new walls(527,610,10,95);
  wall2 = new walls(615,610,10,95);
 ball1=new ball(185,635,27);
-
+d1= createSprite(570,603,30,30);
+d1.addImage(dimg,"da")
 Engine.run(engine);
   
 }
 
 
-function draw() {
-//console.log(mouseX+" "+mouseY)  
+ function draw() {
+ console.log(mouseX+" "+mouseY)  
 
    rectMode(CENTER);
   Engine.update(engine);
   background("white");
-  ground.display();
   wall1.display();
- wall2.display();
+  wall2.display();
+  drawSprites();
+  ground.display();
  ball1.display();
- console.log(ball1.body.position)
- drawSprites();
+ 
+ //console.log(ball1.body.position)
 
 
-}
+ }
 
-function keyPressed(){
+ function keyPressed(){
 
 if (keyCode === UP_ARROW){
 
